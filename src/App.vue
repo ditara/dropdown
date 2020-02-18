708da9e7a9df24987cb629088d79baa941106de4
unknown <dhitta28.dr@gmail.com>
2020-02-18 18:26:41 +0700

    dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Dropdown/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dropdown from './components/Dropdown.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Dropdown
   }
 }
 </script>
